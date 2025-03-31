0b03ed7148dae6072d4234746b655d1baa1f84d7
Kimmilk <Kim244969@gmail.com>
2025-03-31 11:32:23 +0700

    update_cart_favorite

--- lib/provider/favorite_provider.dart
+++ lib/provider/favorite_provider.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:multi_store/data/model/favorite.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final favoriteProvider =
     StateNotifierProvider<FavoriteNotifier, Map<String, Favorite>>((ref) {
@@ -7,7 +10,42 @@ final favoriteProvider =
 });
 
 class FavoriteNotifier extends StateNotifier<Map<String, Favorite>> {
-  FavoriteNotifier() : super({});
+  FavoriteNotifier() : super({}) {
+    _loadFavorite();
+  }
+  // Phương thức riêng tư đển load item từ danh sách yêu thích
+  Future<void> _loadFavorite() async {
+    // Lấy sự yêu thích để lưu trữ trong dữ liệu
+    final prefs = await SharedPreferences.getInstance();
+    // Nạp chuỗi Json của các mục yêu thích từ danh sách yêu thích được chia sẽ dưới key favorite
+    final favoriteString = prefs.getString('favorites');
+    // Kiểm tra nếu String không được null , nghĩa là data lưu trữ
+    if (favoriteString != null) {
+      // Giải mã json String vào Map of dymaic data
+      final Map<String, dynamic> favoriteMap = jsonDecode(favoriteString);
+
+      //convert the dynamic map into map of favorite objsect using fromjson factory method
+      final favorites = favoriteMap.map((key, value) {
+        final favoriteData = value is String ? jsonDecode(value) : value;
+        return MapEntry(key, Favorite.fromJson(favoriteData));
+      });
+
+      //updaing the state with the loaded favorites
+      state = favorites;
+    }
+  }
+
+  // Phương thức riêng tư để lưu danh sách hiện tại yêu thích
+  Future<void> _saveFavorites() async {
+    // Lấy sự yêu thích để lưu trữ trong dữ liệu
+    final prefs = await SharedPreferences.getInstance();
+    // Mã hoá trạng thái hiện tại (Map of favorite object) into json String
+    final favoriteString = jsonEncode(
+      state.map((key, value) => MapEntry(key, value.toMap())),
+    );
+    //saving the json string đến sở thích được chia sẽ với the key "favorites"
+    await prefs.setString('favorites', favoriteString);
+  }
 
   void addProductToFavorite({
     required String productId,
@@ -35,6 +73,7 @@ class FavoriteNotifier extends StateNotifier<Map<String, Favorite>> {
 
     // thông báo cho người dùng trạng thái đã thay đổi
     state = {...state};
+    _saveFavorites();
   }
 
   // Xoa san pham khoi gio hang
@@ -42,6 +81,7 @@ class FavoriteNotifier extends StateNotifier<Map<String, Favorite>> {
     state.remove(productId);
     // trang thai  sau thay doi
     state = {...state};
+    _saveFavorites();
   }
 
   Map<String, Favorite> get getFavortiteItems => state;
